f74a73ffa4a145b10a40100ebeb388afee859170
Xuansheng Wu <wuxsmail@163.com>
2023-05-11 16:18:28 -0400

    Update auto_report.py

--- src/auto_report.py
+++ src/auto_report.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     src = sys.argv[1]
     report = Report(Ks=[1, 5, 10, 20, 50, 100])
     report.parse_logfile(src, "gauc")
-    report.summary(agg=np.mean, groupby=["Model", "Shot", "Dataset"]).to_csv(src.replace(".log", "_mean.tsv"), sep="\t")
-    report.summary(agg=np.std, groupby=["Model", "Shot", "Dataset"]).to_csv(src.replace(".log", "_std.tsv"), sep="\t")
+    report.summary(agg=np.mean, groupby=["Model", "Dataset"]).to_csv(src.replace(".log", "_mean.tsv"), sep="\t")
+    report.summary(agg=np.std, groupby=["Model", "Dataset"]).to_csv(src.replace(".log", "_std.tsv"), sep="\t")
